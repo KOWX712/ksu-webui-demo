b7cca49865f19a823cdb5a95d54dcfc98aac473b
KOWX712 <leecc0503@gmail.com>
2026-04-17 20:20:08 +0800

    module: suport hot install

--- module/customize.sh
+++ module/customize.sh
@@ -1,3 +1,5 @@
+export MODULE_HOT_INSTALL_REQUEST="true" # Remove this if your module need restart to work properly
+
 if [ -z "$MAGISK_VER_CODE" ]; then
     # Hide action.sh if not on Magisk
     mv $MODPATH/action.sh $MODPATH/launch.sh
